f40bfeb6210c211c850ee4dda768f61169f820cc
Mechanical Max <93453184+MechanicalMax@users.noreply.github.com>
2023-12-05 22:44:40 -0500

    Solve d5 Part B

--- AoC23d5.py
+++ AoC23d5.py
@@ -16,13 +16,6 @@ def getMapInfo(mapLines):
 
     return maps
 
-def getGraph(maps):
-    graph =  {}
-
-
-
-    return graph
-
 def partA(lines):
     lowest = 1000000000000000
     seeds = lines[0][7:].strip().split(" ")
@@ -50,24 +43,52 @@ def partB(lines):
     seedRanges.sort()
     
     maps = getMapInfo(lines)
-    graph = getGraph(maps)
     
-    #foundSeed = False
-#
-    #while not foundSeed:
-    #for curMap in reversed(maps):
-    #    for rangeInfo in curMap:
-    #        print(f"\tInput: {rangeInfo[1]} to {rangeInfo[1]+rangeInfo[2]-1}")
-    #        print(f"\t\tOutput: {rangeInfo[0]} to {rangeInfo[0]+rangeInfo[2]-1}")
-    #        if goalNum in range(rangeInfo[0],rangeInfo[0]+rangeInfo[2]):
-    #            goalNum = rangeInfo[1]
-    #    print("Next Map=")
-    #    print(goalNum)
-#
-    #for i in seedRanges:
-    #    print(i)
-        
-    return graph
+    # Credit for this logic goes to HyperNeutrino,
+    # I was still able to use my data structures,
+    # But the range math is from
+    # https://www.youtube.com/watch?v=NmxHw_bHhGM
+    # I tried to understand everything first instead
+    # of blindly copying.
+
+    # Strategy:
+    # For every seed map
+    for seedMap in maps:
+        # keep track of ranges already mapped
+        mappedSeeds = []
+        # while there are still ranges to be mapped
+        while len(seedRanges) > 0:
+            # Get a range start and end from seedRanges
+            start, end = seedRanges.pop()
+            # For every range in the map
+            for rangeInfo in seedMap:
+                # Check for overlap endpoints
+                overlapStart = max(start, rangeInfo[1])
+                overlapEnd = min(end, rangeInfo[1] + rangeInfo[2])
+                # If there is a valid overlap
+                if overlapStart < overlapEnd:
+                    # Map the overlap and save in mapped seeds
+                    mappedSeeds.append((overlapStart + rangeInfo[0] - rangeInfo[1], overlapEnd + rangeInfo[0] - rangeInfo[1]))
+                    ## Check if there needs to be an additional range checked in seedRanges
+                    #current start is within start
+                    if overlapStart > start:
+                        seedRanges.append((start, overlapStart))
+                    #current end is within end
+                    if overlapEnd < end:
+                        seedRanges.append((overlapEnd, end))
+                    break
+            else:
+                # if nothing else applies, it is out of range
+                # So, don't apply a transformation to current
+                # and save to mapped seeds
+                mappedSeeds.append((start,end))
+        # set mapped seeds to seedRanges to repeat process
+        seedRanges = mappedSeeds
+
+    # Lowest possible seed will be the minimum value of the lowest range
+    lowest = sorted(seedRanges)[0][0]
+    
+    return lowest
 
 print(partA(inputLines))
-print(partB(inputLines)) ## Too much to optimize during finals
+print(partB(inputLines)) ## Too much to optimize during finals; Thank you HyperNeutrino for the help!
